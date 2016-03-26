3979fbf78f93c3772d09b642cd59fd7346290248
Terry Marr <terrymarr280@gmail.com>
2016-03-26 11:36:13 -0700

    save1

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,13 +1,9 @@
-/**
- * Created by jj on 12/23/15.
- */
-
 var React = require('react');
 
 // to compile styles
 var Styles = require('../css/app.scss');
 
 React.render(
-    <h1>Hello, JJ's world. :) </h1>,
+    <h1>Hello, world. :) </h1>,
     document.body
 );
